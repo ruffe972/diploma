fafe5d0394e25a48750ccbd3feb3cd5721682235
Ivan Ershov <ivanershov51@gmail.com>
2018-09-30 17:45:24 +0300

    Add dummy RecyclerView

--- app/src/main/java/ivan/launcherforchildren/MainActivity.kt
+++ app/src/main/java/ivan/launcherforchildren/MainActivity.kt
@@ -2,11 +2,15 @@ package ivan.launcherforchildren
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ivan.launcherforchildren.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ItemFragment.OnListFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
     }
 }
